3ba607ce27c112e21bda85631c9f095a7cddedcf
Émile Boucher <emileboucheracc@gmail.com>
2025-05-13 03:37:22 -0400

    [devops] debug infos

--- build_msi.ps1
+++ build_msi.ps1
@@ -1,8 +1,23 @@
+Write-Host "VERSIONS"
+Write-Host "================="
+rustc --version
+cargo --version
+Write-Host "npm $(npm --version)"
+Write-Host "nodejs $(node --version)"
+Write-Host "-----------------"
+
 Write-Host "Building All Projects"
 cargo build --workspace --release
 
-Write-Host "Building Msi"
+Write-Host "Move to $(Get-Location)/gui"
 $previous = Get-Location
 cd gui
+
+Write-Host "Get Npm deps"
+npm i
+
+Write-Host "Building Msi"
 npm run tauri build -- --release
+
+Write-Host "Move back to $($previous)"
 cd $previous
